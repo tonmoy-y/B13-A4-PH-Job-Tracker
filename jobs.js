e872376423dd0516472437e708c91f2ff2c46e35
tonmoy-y <tonmoysarker451@gmail.com>
2026-03-08 19:33:41 +0600

    delete functionality added

--- jobs.js
+++ jobs.js
@@ -1,1 +1,42 @@
 console.log("Job Tracker Connected");
+
+let deleteJobs = 0;
+const totalJobElement = document.getElementsByClassName('total-job');
+let totalJobs = parseInt(totalJobElement[0].innerText);
+console.log(parseInt(totalJobElement[0].innerText));
+const NoJobElement = document.getElementById('no-job');
+console.log(NoJobElement);
+
+
+
+
+
+function addToInterview(id) {
+    let interviewJobs = [];
+    showNoJob(interviewJobs.length);
+}
+
+
+
+
+
+//------------------------- delete jobs ----------------------------- 
+function deleteJob(jobId) {
+    const jobCard = document.getElementById(jobId);
+    if (jobCard) {
+        jobCard.remove();
+    }
+    deleteJobs++;
+    let jobLeft = totalJobs - deleteJobs;
+    console.log(jobLeft);
+    showNoJob(jobLeft);
+    totalJobElement[0].innerText = jobLeft;
+    totalJobElement[1].innerText = jobLeft;
+}
+
+
+function showNoJob (num) {
+    if(num === 0 ) {
+        NoJobElement.classList.remove('hidden');
+    }
+}
